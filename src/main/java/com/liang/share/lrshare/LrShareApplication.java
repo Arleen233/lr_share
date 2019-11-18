3fae0dba5bc478e6efa937070dc510995c70b434
chenqiuxia123 <1501766508@qq.com>
2019-11-18 15:49:36 +0800

    创建项目

---
+++ src/main/java/com/liang/share/lrshare/LrShareApplication.java
@@ -0,0 +1,13 @@
+package com.liang.share.lrshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LrShareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LrShareApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/liang/share/lrshare/LrShareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liang.share.lrshare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LrShareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
